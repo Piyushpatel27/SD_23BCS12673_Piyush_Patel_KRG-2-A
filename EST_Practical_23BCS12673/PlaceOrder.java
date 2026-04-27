431e6acb324b0b81a8284260740e6a8ea0f237a1
Piyush Patel <piyushpatel0270@gmail.com>
2026-04-27 16:32:15 +0530

    EST-Practical

---
+++ EST_Practical_23BCS12673/PlaceOrder.java
@@ -0,0 +1,19 @@
+// Order-Facade
+
+public class PlaceOrder{
+    public void inventoryCheck(){
+        System.out.println("checks inventory.");
+    }
+    public void paymentGateway(){
+        System.out.println("checks payment status.");
+    }
+    public void NotificationService(){
+        System.out.println("handles email/sms notification for order .");
+    }
+}
+public class PlaceOrder{
+    public static void main(String []args){
+        //create function
+    }
+    
+}
